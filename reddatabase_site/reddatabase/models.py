6f83608c3fe72d3ea114ae4c965f45b85069f2e3
kbradner112@gmail.com <kbradner112@gmail.com>
2016-12-03 16:06:34 -0500

    models and so on

--- reddatabase_site/reddatabase/models.py
+++ reddatabase_site/reddatabase/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class submission(models.Model):
-	postid = models.CharField(max_length=6),(primary_key=True)
+	postid = models.CharField(max_length=6, primary_key=True)
 	username = models.CharField(max_length=20)
 	subredditName = models.CharField(max_length=20)
 	title = models.CharField(max_length=300)
@@ -13,21 +13,24 @@ class submission(models.Model):
 	timeSubmitted = models.DateTimeField
 
 class Comment(models.Model):
-	cid = models.CharField(max_length=6)(,primary_key=True)
+	cid = models.CharField(max_length=6, primary_key=True)
 	p_cid = models.CharField(max_length=6)
 	postid = models.CharField(max_length=6)
-	text = models.CharFIeld(max_length=40000)
-	username = models.CharFIeld(max_length=20)
+	text = models.CharField(max_length=40000)
+	username = models.CharField(max_length=20)
 	score = models.IntegerField
 	commentType = models.IntegerField
 	timeSubmitted = models.DateTimeField
 
 class moderator(models.Model):
-	username = username = models.CharField(max_length=20),(primary_key=True)
-	subredditName = models.CharField(max_length=20),(primary_key=True)
+	username = username = models.CharField(max_length=20)#, primary_key=True)
+	subredditName = models.CharField(max_length=20)#, primary_key=True)
+	
+	class Meta:
+		unique_together (("migration", "host"),)
 
 class user(models.Model):
-	username = models.CharField(max_length=20),(primary_key=True)
+	username = models.CharField(max_length=20, primary_key=True)
 	karma = models.IntegerField
 
 class subreddit(models.Model):
@@ -41,4 +44,12 @@ class linkpost(models.Model):
 class textpost(models.Model):
 	postid = models.CharField(max_length=6, primary_key=True)
 	content = models.CharField(max_length=40000)
- your models here.
+
+class submission_hasa_subreddit(models.Model):
+	postid = models.CharField(max_length=6)
+	subredditName = models.CharField(max_length=20)
+
+	class Meta:
+		unique_together (("postid", "subredditName"),)
+
+#your models here.
