4b1e4e50eef60ec5736acaf6f40f039d93998256
kbradner112@gmail.com <kbradner112@gmail.com>
2016-12-09 21:02:37 -0500

    added a test query

--- reddatabase_site/reddatabase/views.py
+++ reddatabase_site/reddatabase/views.py
@@ -8,6 +8,10 @@ def index(request):
 	context = {}
 	return HttpResponse(template.render(context, request))
 
-
+def number_moderators(self):
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT COUNT(*) FROM reddatabase_user U, subreddit_hasa_user S WHERE U.username = S.username AND S.subredditName = countryName")
+		rows = cursor.namedtuplefetchall()
+	return rows
 
 # Create your views here.i
