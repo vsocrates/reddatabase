a738383ed4f3f1bd1d35f6be3da8e97b832b5bbd
Vimig Socrates <vimig.socrates@gmail.com>
2016-12-09 22:31:23 -0500

    added support for num mods in popup

--- reddatabase_site/reddatabase/views.py
+++ reddatabase_site/reddatabase/views.py
@@ -5,14 +5,14 @@ from django.template import loader
 from django.db import connection
 
 def index(request):
-	num_mods = number_moderators()
+	#num_mods = number_moderators()
 	template = loader.get_template('reddatabase/index.html')
-	context = {'num_mods':num_mods}
+	context = {}#= {'num_mods':num_mods}
 	return HttpResponse(template.render(context, request))
 
 def number_moderators():
 	with connection.cursor() as cursor:
-		cursor.execute("SELECT COUNT(*) FROM reddatabase_user U, subreddit_hasa_user S WHERE U.username = S.username AND S.subredditName = countryName")
+		cursor.execute("SELECT COUNT(*) FROM reddatabase_user U, subreddit_hasa_user S WHERE U.username = S.username AND S.subredditName = Canada")
 		rows = cursor.namedtuplefetchall()
 	return rows
 
