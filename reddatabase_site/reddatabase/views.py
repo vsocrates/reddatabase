1ece1eaa523a89f5a77d834f703104623b67afc3
Vimig Socrates <vimig.socrates@gmail.com>
2016-12-09 21:09:24 -0500

    added context dict in index

--- reddatabase_site/reddatabase/views.py
+++ reddatabase_site/reddatabase/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from django.db import connection
 
 def index(request):
+	num_mods = number_moderators()
 	template = loader.get_template('reddatabase/index.html')
-	context = {}
+	context = {'num_mods':num_mods}
 	return HttpResponse(template.render(context, request))
 
-def number_moderators(self):
+def number_moderators():
 	with connection.cursor() as cursor:
 		cursor.execute("SELECT COUNT(*) FROM reddatabase_user U, subreddit_hasa_user S WHERE U.username = S.username AND S.subredditName = countryName")
 		rows = cursor.namedtuplefetchall()
